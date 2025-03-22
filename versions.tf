d7eeebd8a649ba2f12d59ba8f4b6969a29d9e2a0
natalia.gassis <natalia.gassis@luizalabs.com>
2025-03-22 17:37:56 -0300

    fix: simplificar configuração do backend S3 no arquivo versions.tf

--- versions.tf
+++ versions.tf
@@ -10,15 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "granato-tf"
-    key    = "kubernetes/terraform.tfstate"
-    region = "br-se1"
-    #    skip_region_validation      = true
-    #    skip_credentials_validation = true
-    #    skip_requesting_account_id  = true
-    #    skip_s3_checksum            = true
-    endpoint = {
-      s3 = "https://br-se1.magaluobjects.com/granato-tf"
-    }
-  }
+    bucket   = "granato-tf"
+    key      = "kubernetes/terraform.tfstate"
+    region   = "br-se1"
+    endpoint = "https://br-se1.magaluobjects.com/granato-tf" 
 }
